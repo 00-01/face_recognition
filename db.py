6b3db3af0e1691af6b105fae65ecfbf7cb4d61d1
00-01 <59557934+00-01@users.noreply.github.com>
2021-04-27 16:48:29 +0900

    new

--- db.py
+++ db.py
@@ -6,8 +6,5 @@ def access_db():
     db = client['face_id']
     col = db['id']
     error_col = db['error']
-    db.col.createIndexe({'name':1, 'id':1},{unique:True})
-    # db.col.createIndex({'id':1},{unique:True})
-    return client, db, col, error_col
 
-access_db()
+    return client, db, col, error_col
